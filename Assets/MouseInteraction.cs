fb6921a6f5fcc59f1e84794f5aa60252c22ba54c
ludwig meiton <moltenmagnesiumcore@gmail.com>
2024-07-12 14:46:12 +0200

    trying RAYS

---
+++ Assets/MouseInteraction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MouseInteraction : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        // i need to learn ray casts
+    }
+}
